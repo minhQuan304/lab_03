34a69180de15e37bb33d24473782a656d4dd8f74
minhQuan304 <thaiminhquan58@gmail.com>
2024-11-26 08:35:37 +0700

    printcard

--- src/Aims/Aims.java
+++ src/Aims/Aims.java
@@ -2,23 +2,24 @@ package Aims;
 
 public class Aims {
 	public static void main(String[] args) {
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 120, 24.95f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Cinderella", "Animation", "Clyde Geronimi", 85, 15.95f);
+
+		// Create a Cart
 		Cart anOrder = new Cart();
 
-		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-		System.out.println("DVD 1 ID: " + dvd1.getId()); // In ra ID của DVD1
+		// Add DVDs to the cart
 		anOrder.addDigitalVideoDisc(dvd1);
-
-		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Science Fiction", "George Lucas", 87, 24.95f);
-		System.out.println("DVD 2 ID: " + dvd2.getId()); // In ra ID của DVD2
 		anOrder.addDigitalVideoDisc(dvd2);
+		anOrder.addDigitalVideoDisc(dvd3);
 
-		System.out.println("Total DVDs created: " + DigitalVideoDisc.getNbDigitalVideoDiscs()); // In ra tổng số DVD đã
-																								// tạo
-		System.out.println("Total cost = " + anOrder.totalCost());
+		// Print Cart details
+		anOrder.printCartDetails(); // This will print the cart details
 
+		// Remove a DVD and print the cart again
 		anOrder.removeDigitalVideoDisc(dvd2);
-		System.out.println("Total cost after removal = " + anOrder.totalCost());
-
+		anOrder.printCartDetails(); // This will print the updated cart details
 	}
 
 }
